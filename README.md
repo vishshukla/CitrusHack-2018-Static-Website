f3c9934e83a8d346fa86727f279440c4114c2ab7
vishshukla <vishwas.shukla@outlook.com>
2018-04-09 19:03:16 -0700

    added timeline functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+# The Live Website of Citrus Hack
+---
+Citrus Hack 2018
--- index.js
+++ index.js
@@ -1,8 +1,42 @@
-console.log("IN JS");
+(function() {
+
+  'use strict';
+
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+
+})();
+
 var countDownDate = new Date("Apr 22, 2018 12:00:00").getTime();
 
   var countDownFunction = setInterval(function() {
       var now = new Date().getTime();
+
 
       var distance = countDownDate - now;
 
@@ -11,13 +45,13 @@ var countDownDate = new Date("Apr 22, 2018 12:00:00").getTime();
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
+      document.getElementById("timer").innerHTML = "Time Left\n" + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+      if(distance > (1000 * 60 * 60 * 24)) {
+        document.getElementById("timer").innerHTML = "Countdown Till Hackathon!" + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+      }
       if(distance < 0) {
         clearInterval(countDownFunction);
-        document.getElementById("demo").innerHTML = "Time to Choose Your Representatives!";
+        document.getElementById("demo").innerHTML = "HACKING OVER";
       }
-      
-
 
   },1000);
