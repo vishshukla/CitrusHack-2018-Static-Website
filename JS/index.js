094ab46d422cbccdb0b51b5d2964356f9f15573c
ptumb001 <ptumb001@ucr.edu>
2018-04-20 13:55:00 -0700

    Fixed submodule error

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'JS/particles.js/particles.json', function() {
+particlesJS.load('particles-js', 'JS/particles/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
